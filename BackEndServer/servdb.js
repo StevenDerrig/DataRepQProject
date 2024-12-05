6dda3872afd9558677f92186916ab13d6ff50620
Steebo <G00424644@atu.ie>
2024-12-05 23:06:54 +0000

    Installed MongoDB functionality.

--- BackEndServer/servdb.js
+++ BackEndServer/servdb.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 4000;
 
 //database connection and setup
+const mongoose = require('mongoose');
+mongoose.connect('');
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
